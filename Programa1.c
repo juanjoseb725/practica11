a47fe87fdabb5ce085d8cbba1d73867b7dbe19ff
juanjoseb725 <juanjoseb725@gmail.com>
2026-04-30 13:39:22 -0600

    Add Programa1.c to print string in reverse

---
+++ Programa1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+// Prototipo de la función  
+void imprimir(char s[]);
+
+int main() {
+    // Declaración de la cadena
+    char nombre[] = "Facultad de Ingeniería";
+    
+    // Llamada a la función
+    imprimir(nombre);
+
+    return 0;
+}
+
+// Implementación de la función: Imprime el string de atrás hacia adelante
+void imprimir(char s[]) {
+    int tam;
+
+    // Recorremos el arreglo desde la última posición válida hasta la cero
+    for (tam = strlen(s) - 1; tam >= 0; tam--) {
+        printf("%c", s[tam]);
+    }
+
+    printf("\n");
+}
